9bc83ebb1a843671731d5795dc48f952565aa905
Nick Day <thenickday@icloud.com>
2019-11-26 23:32:22 -0500

    added imports for Bomb, HW & RG,

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
-import "./App.css";
-import Messages from "./Messages";
-import TheDate from "./state/TheDate";
-import Counter from "./state/Counter";
+// import "./App.css";
+// import Messages from "./Messages";
+// import TheDate from "./state/TheDate";
+// import Counter from "./state/Counter";
+import Bomb from "./state-drills/Bomb";
+import HelloWorld from "./state-drills/HelloWorld";
+import RouletteGun from "./state-drills/RouletteGun";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>YOUR APPLICATION NAME!</h1>
+      <div className="App">        
+        <p><Bomb /></p>
+        <br />
+        <p><HelloWorld /></p>
+        <br />
+        <p><RouletteGun /></p>
+        {/* <h1>YOUR APPLICATION NAME!</h1>
         <Counter count={123} />
         <TheDate />
         <Messages name="Messages" unread={0} />
         <Messages name="Notifications" unread={10} />
-        <div>Hello!</div>        
+        <div>Hello!</div> */}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import Bomb from "./state-drills/Bomb";
+import HelloWorld from "./state-drills/HelloWorld";
+import RouletteGun from "./state-drills/RouletteGun";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
