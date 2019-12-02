28b1bf7011aa0e3311ca3840e1073214ddcb8970
Nick Day <thenickday@icloud.com>
2019-12-01 21:47:03 -0500

    updated main app and index

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from "react";
-// import "./App.css";
+import "./App.css";
 // import Messages from "./Messages";
 // import TheDate from "./state/TheDate";
 // import Counter from "./state/Counter";
-import Bomb from "./state-drills/Bomb";
-import HelloWorld from "./state-drills/HelloWorld";
-import RouletteGun from "./state-drills/RouletteGun";
+// import Bomb from "./state-drills/Bomb";
+// import HelloWorld from "./state-drills/HelloWorld";
+// import RouletteGun from "./state-drills/RouletteGun";
+import Accordion from './state-drills/Accordion';
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">        
-        <p><Bomb /></p>
+      <div className="App">
+        <h1><Accordion /></h1>
+        <Accordion sections={sections} />
+        {/* <p><Bomb /></p>
         <br />
         <p><HelloWorld /></p>
         <br />
-        <p><RouletteGun /></p>
+        <p><RouletteGun /></p> */}
         {/* <h1>YOUR APPLICATION NAME!</h1>
         <Counter count={123} />
         <TheDate />
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import Bomb from "./state-drills/Bomb";
-import HelloWorld from "./state-drills/HelloWorld";
-import RouletteGun from "./state-drills/RouletteGun";
+// import Bomb from "./state-drills/Bomb";
+// import HelloWorld from "./state-drills/HelloWorld";
+// import RouletteGun from "./state-drills/RouletteGun";
 
 ReactDOM.render(<App />, document.getElementById("root"));
