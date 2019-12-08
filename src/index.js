c061d17225d77403e759ace2b5f30666ac14cbcb
Nick Day <thenickday@icloud.com>
2019-12-08 16:45:44 -0500

    added new folder w/contents, updated index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import App from './paltrows-power-toes/App';
 // import Bomb from "./state-drills/Bomb";
 // import HelloWorld from "./state-drills/HelloWorld";
 // import RouletteGun from "./state-drills/RouletteGun";
