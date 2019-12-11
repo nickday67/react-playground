f86d9598afddf762347a144cdde59473a0dd54e7
Nick Day <thenickday@icloud.com>
2019-12-11 15:55:55 -0500

    comment out & adding new practice

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from './paltrows-power-toes/App';
+import AppLang from './lang-context/AppLang';
+// import { BrowserRouter } from "react-router-dom";
+// import App from './paltrows-power-toes/App';
 // import Bomb from "./state-drills/Bomb";
 // import HelloWorld from "./state-drills/HelloWorld";
 // import RouletteGun from "./state-drills/RouletteGun";
 
+// ReactDOM.render(
+// <BrowserRouter>
+//     <App />
+// </BrowserRouter>, 
+// document.getElementById("root")
+// );
+
 ReactDOM.render(
-<BrowserRouter>
-    <App />
-</BrowserRouter>, 
-document.getElementById("root")
+    <AppLang />,
+    document.getElementById('root')
 );
