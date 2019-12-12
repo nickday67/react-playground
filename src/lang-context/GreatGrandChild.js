7fd5dfe5a7066d59111140a786e0c9eaff9c3cc2
Nick Day <thenickday@icloud.com>
2019-12-11 19:51:39 -0500

    more stuff

--- src/lang-context/GreatGrandChild.js
+++ src/lang-context/GreatGrandChild.js
@@ -6,7 +6,7 @@ class GreatGrandChild extends Component {
   static contextType = LanguageContext;
 
   render() {
-    const copy = languageSpecificCopy["en-US"] || {};
+    const copy = languageSpecificCopy[this.context.lang] || {};
     return (
       <section>
         <h2>{copy.title}</h2>
